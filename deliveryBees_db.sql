c026d142f9c4d82c934eb0712df15c6f2f5274e6
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-04-12 22:58:56 +0000

    database

--- deliveryBees_db.sql
+++ deliveryBees_db.sql
@@ -29,7 +29,35 @@ CREATE TABLE dbo.SenderAddress(
 
 );
 
+CREATE TABLE Package(
+	packageID INT NOT NULL,
+	deliveryID INT NOT NULL,
+	courierID INT NOT NULL,
+	recieptAddressID INT NOT NULL,
+	pWeight DECIMAL(10,2) NOT NULL,
+	price DECIMAL(10,2) NOT NULL
+);
 
+CREATE TABLE DeliveryInformation(
+	deliveryID INT NOT NULL,
+	deliveryDate DATE NOT NULL
+);
+
+CREATE TABLE recieptAddress(
+	recieptAddressID INT NOT NULL,
+	street VARCHAR(100) NOT NULL,
+	houseNumber VARCHAR(20) NOT NULL,
+	city VARCHAR(70) NOT NULL,
+	country VARCHAR(70) NOT NULL
+);
+
+CREATE TABLE courier(
+	vehicleID INT NOT NULL,
+	courierID INT NOT NULL,
+	licenseNumber VARCHAR(100) NOT NULL
+);
+
+-- DATABASE POPULATION --
 
 -- Insert rows into table 'Courier'
 INSERT INTO dbo.Courier VALUES
@@ -51,5 +79,53 @@ INSERT INTO dbo.Courier VALUES
    ( N'Abele Street', N'AS89', N'Community 9'),
    ( N'Emmause Lane', N'78EL89', N'Community 25')
 
+
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (1,"lioness","North","130 kg","40 kg"); 
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (2,"tiger","South","246 kg","58 kg"); 
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (3,"lion","North","190 kg","40 kg"); 
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (4,"cheetah","East","65 kg","25 kg"); 
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (5,"leopard","East","31 kg","20 kg"); 
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (6,"jaguar","East","90 kg","30 kg"); 
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (7,"puma","West","93 kg","31 kg"); 
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (8,"tiger","South","246 kg","58 kg"); 
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (9,"panther","West","88 kg","29 kg"); 
+INSERT INTO Package(packageID, deliveryID, courierID, recieptAddressID, pWeight, price) VALUES (10,"lion","North","190 kg","40 kg"); 
+
+
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (1,1,"Online");  
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (2,2,"Online");  
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (3,3,"In-person");  
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (4,4,"Online");  
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (5,5,"In-person");  
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (6,6,"In-person");  
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (7,7,"In-person");  
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (8,8,"Online");  
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (9,9,"In-person");  
+INSERT INTO DeliveryInformation(deliveryID, deliveryDate) VALUES (10,10,"Online");  
+
+
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("Kwame Ato","+233554327586","Burma Camp - Accra","Speedy");  
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("Lizbeth Cole","+233574827586","Awosie - Accra","Calcius");  
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("Mary Otchere","+233248576586","Airport Residential Area - Accra","Kiera");  
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("Yaw Kross","+233244327586","Spintex - Accra","Infinity");  
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("Delilah Jones","+233234757586","Teshie Nungua - Accra","Amba");  
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("Vera Akwei","+233559748328","Dansoman(SSNIT) - Accra","Bridge"); 
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("John Kennedy","+23323982785","Tse-Addo - Accra", "Killer");  
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("Eyram Adjovi","+233279823579","Trassaco-Valley(Plot 1) - Accra","Ariel");  
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("Nii Amon Atukweifio","+233268794125","Ashongmang Estate - Accra","Aliki");  
+INSERT INTO recieptAddress(recieptAddressID, street, houseNumber, city, country) VALUES ("Erica Sackey","+233279842578","East-Airport - Accra","Corvette"); 
+ 
+
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (1,1,"Online");  
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (2,2,"Online");  
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (3,3,"In-person");  
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (4,4,"Online");  
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (5,5,"In-person");  
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (6,6,"In-person");  
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (7,7,"In-person");  
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (8,8,"Online");  
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (9,9,"In-person");  
+INSERT INTO courier(vehicleID, courierID, licenseNumber) VALUES (10,10,"Online");  
+ 
 
 GO
