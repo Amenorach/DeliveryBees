412f76fbea0b60c6a00eb714bce38bf416ad7872
Akosua-Debrah <takyiwaadebrah@gmail.com>
2023-04-16 19:29:10 +0000

    Update deliveryBees_db.sql

--- deliveryBees_db.sql
+++ deliveryBees_db.sql
@@ -6,10 +6,10 @@ USE deliveryBees;
 -- Create a new table called ‘Courier Details' in schema 'dbo' --
 -- Drop the table if it already exists --
 
-DROP TABLE IF EXISTS dboCourier;
+DROP TABLE IF EXISTS Courier;
 
 
-CREATE TABLE dboCourier (
+CREATE TABLE Courier (
    CourierId        INT    NOT NULL  PRIMARY KEY, 
    Rider_fname      VARCHAR (10)  NOT NULL,
    Rider_lname  VARCHAR(10)  NOT NULL,
@@ -26,35 +26,35 @@ CREATE TABLE customer (
   cust_email VARCHAR(100) NOT NULL,
   cust_phone VARCHAR(20),
   address VARCHAR (50),
-  cust_pass VARCHAR (40) NOT NULL,
+  cust_pass VARCHAR (40) NOT NULL UNIQUE,
   user_role int(11) NOT NULL
 );
 
-DROP TABLE IF EXISTS dboSender;
+DROP TABLE IF EXISTS Sender;
 
-CREATE TABLE dboSender (
+CREATE TABLE Sender (
+	senderID INT NOT NULL PRIMARY KEY,
     cust_id INT,
     FOREIGN KEY (cust_id) REFERENCES customer(cust_id)
 );
 
 
-DROP TABLE IF EXISTS dboSenderAddress;
+DROP TABLE IF EXISTS SenderAddress;
 
-CREATE TABLE dboSenderAddress(
+CREATE TABLE SenderAddress(
 	Street VARCHAR (50)  NOT NULL,
 	houseNumber TINYINT(10)  NOT NULL,
 	City VARCHAR (50)  NOT NULL,
-	Country   CHAR (50)  NOT NULL,
-	customerID INT,
-    FOREIGN KEY (customerID) REFERENCES customer(cust_id)
+	senderID INT,
+    FOREIGN KEY (senderID) REFERENCES Sender(senderID)
 );
 
 
 
 CREATE TABLE recipient (
   recipientID INT NOT NULL PRIMARY KEY,
-  customerID INT NOT NULL,
-  FOREIGN KEY (customerID) REFERENCES customer(cust_id),
+  cust_id INT NOT NULL,
+  FOREIGN KEY (cust_id) REFERENCES customer(cust_id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -70,7 +70,10 @@ CREATE TABLE recipient (
 
 CREATE TABLE DeliveryInformation(
 	deliveryID INT NOT NULL PRIMARY KEY,
-	deliveryDate DATE NOT NULL
+	deliveryDate DATE NOT NULL,
+    deliverystatus enum ("at the warehouse","In transit", "Delivered"),
+    CourierId INT,
+    FOREIGN KEY (CourierId) REFERENCES Courier (CourierId)
 );
 
 CREATE TABLE recipientAddress(
@@ -78,16 +81,15 @@ CREATE TABLE recipientAddress(
 	street VARCHAR(100) NOT NULL,
 	houseNumber VARCHAR(20) NOT NULL PRIMARY KEY,
 	city VARCHAR(70) NOT NULL,
-	country VARCHAR(70) NOT NULL,
 	FOREIGN KEY (recipientID) REFERENCES recipient(recipientID)
 );
 
-CREATE TABLE courier(
+CREATE TABLE couriervehiclerider(
 	vehicleID INT NOT NULL,
 	courierId INT NOT NULL,
 	licenseNumber VARCHAR(100) NOT NULL,
 	FOREIGN KEY (vehicleID) REFERENCES vehicle(id),
-    FOREIGN KEY (courierId) REFERENCES dboCourier(courierId)
+    FOREIGN KEY (courierId) REFERENCES Courier(courierId)
 );
 
 CREATE TABLE Package(
@@ -115,6 +117,17 @@ CREATE TABLE payment (
   on delete cascade
 );
 
+CREATE TABLE courierAvailabilty(
+	courierId INT NOT NULL,
+    start_time TIME,
+    start_date DATE,
+     end_time TIME,
+    end_date DATE,
+    FOREIGN KEY (courierId) REFERENCES Courier(courierId)
+);
+
+
+
 -- CONSTRAINTS--
 
 --
@@ -125,9 +138,9 @@ ADD CONSTRAINT UQ_cust_email UNIQUE (cust_email);
 -- ADD CONSTRAINT CHK_cust_phone CHECK (cust_phone LIKE '[0-9]%');
 
 
-ALTER TABLE dboSenderAddress
-ADD CONSTRAINT FK_SenderAddress_Customer FOREIGN KEY (customerID)
-    REFERENCES customer(cust_id);
+ALTER TABLE SenderAddress
+ADD CONSTRAINT FK_SenderAddress_Customer FOREIGN KEY (senderID)
+    REFERENCES Sender(senderID);
 
 --
 -- Constraints for table `Package`
@@ -136,7 +149,7 @@ ALTER TABLE Package
 ADD CONSTRAINT FK_Package_DeliveryInformation FOREIGN KEY (deliveryID)
         REFERENCES DeliveryInformation(deliveryID),
 ADD CONSTRAINT FK_Package_Courier FOREIGN KEY (courierID)
-        REFERENCES dboCourier(CourierId);
+        REFERENCES Courier(CourierId);
 
 
 --
@@ -144,7 +157,7 @@ ADD CONSTRAINT FK_Package_Courier FOREIGN KEY (courierID)
 --
 
 ALTER TABLE recipient
-ADD CONSTRAINT FK_recipient_Customer FOREIGN KEY (customerID)
+ADD CONSTRAINT FK_recipient_Customer FOREIGN KEY (cust_id)
         REFERENCES customer(cust_id);
 
 
@@ -162,27 +175,27 @@ ADD CONSTRAINT NN_payment_date CHECK (payment_date IS NOT NULL);
 --
 -- Constraints for table `courier`
 --
-ALTER TABLE courier
+ALTER TABLE couriervehiclerider
 -- ADD CONSTRAINT PK_courier PRIMARY KEY (vehicleID, courierID),
 ADD CONSTRAINT UQ_courier_licenseNumber UNIQUE (licenseNumber);
 
 --
 -- DATABASE POPULATION--
 
-INSERT INTO customer (cust_id, cust_fname, cust_lname, cust_email, cust_phone, address, cust_pass, user_role)
+INSERT INTO customer (cust_id, cust_fname, cust_lname, cust_email, cust_phone, address,cust_pass, user_role)
 VALUES
-  (1, 'Kwame', 'Appiah', 'kwame.appiah@example.com', '0241234567', '123 Main St, Accra' , qwerty , 1),
-  (2, 'Adjoa', 'Boateng', 'adjoa.boateng@example.com', '0272345678', '456 Oak St, Kumasi', 123456, 2),
-  (3, 'Yaw', 'Kwakye', 'yaw.kwakye@example.com', '0203456789', '789 Elm St, Cape Coast', asdfg, 1),
-  (4, 'Akua', 'Owusu', 'akua.owusu@example.com', '0244567890', '234 Pine St, Tamale', twenty, 1),
-  (5, 'Kofi', 'Addo', 'kofi.addo@example.com', '0545678901', '567 Maple St, Takoradi', 67890, 1),
-  (6, 'Esi', 'Adu', 'esi.adu@example.com', '0246789012', '890 Cherry St, Accra', tryme1, 1),
-  (7, 'Yaw', 'Asante', 'yaw.asante@example.com', '0277890123', '1234 Elmwood Ave, Kumasi', thankgod, 2),
-  (8, 'Ama', 'Mensah', 'ama.mensah@example.com', '0209012345', '5678 Oakwood Blvd, Cape Coast', pineapple, 1),
-  (9, 'Kwesi', 'Addae', 'kwesi.addae@example.com', '0540123456', '9012 Pine St, Tamale', myphone, 1),
-  (10, 'Abena', 'Osei', 'abena.osei@example.com', '0243456789', '3456 Maple Ave, Takoradi', yourlove, 2);
+  (1, 'Kwame', 'Appiah', 'kwame.appiah@gmail.com', '0241234567', '123 Main St, Accra', '1l0v3k3nk3y', 1),
+  (2, 'Adjoa', 'Boateng', 'adjoa.boateng@gmail.com', '0272345678', '456 Oak St, Kumasi', 'hdsf848rgh', 1),
+  (3, 'Yaw', 'Kwakye', 'yaw.kwakye@gmail.com', '0203456789', '789 Elm St, Cape Coast','idhoayd2094',2),
+  (4, 'Akua', 'Owusu', 'akua.owusu@gmail.com', '0244567890', '234 Pine St, Tamale','7845bf834YGIG',1),
+  (5, 'Kofi', 'Addo', 'kofi.addo@gmail.com', '0545678901', '567 Maple St, Takoradi','893GEU89Y9',2),
+  (6, 'Esi', 'Adu', 'esi.adu@gmail.com', '0246789012', '890 Cherry St, Accra','SIHF6gyig#',2),
+  (7, 'Yaw', 'Asante', 'yaw.asante@gmail.com', '0277890123', '1234 Elmwood Ave, Kumasi','he79y%^',2),
+  (8, 'Ama', 'Mensah', 'ama.mensah@gmail.com', '0209012345', '5678 Oakwood Blvd, Cape Coast','huGHU876',1),
+  (9, 'Kwesi', 'Addae', 'kwesi.addae@gmail.com', '0540123456', '9012 Pine St, Tamale','HIUSAI^*%^',2),
+  (10, 'Abena', 'Osei', 'abena.osei@gmail.com', '0243456789', '3456 Maple Ave, Takoradi','#(#0iudgcsui(660',1);
 
-INSERT INTO recipient (recipientID, customerID) VALUES
+INSERT INTO recipient (recipientID, cust_id) VALUES
   (1, 1),
   (2, 2),
   (3, 3),
@@ -196,7 +209,7 @@ INSERT INTO recipient (recipientID, customerID) VALUES
   
   
 -- Insert rows into table 'Courier'
-INSERT INTO dboCourier (CourierId, Rider_fname, Rider_lname, Rider_number) VALUES
+INSERT INTO Courier (CourierId, Rider_fname, Rider_lname, Rider_number) VALUES
    ( 1, N'Orlando', N'August', N'+233507384211'),
    ( 2, N'Keith', N'Benson', N'+2332709405800'),
    ( 3, N'Donna', N'Carlos', N'+2332648329103'),
@@ -208,19 +221,33 @@ INSERT INTO dboCourier (CourierId, Rider_fname, Rider_lname, Rider_number) VALUE
    ( 9, N'James', N'Johnson', N'+2332400567790'),
    ( 10, N'Emily', N'Wilson', N'+2332648329107');
    
-INSERT INTO dboSenderAddress (Street, houseNumber, City, Country, customerID)
-VALUES ('Adabraka Road', 12, 'Accra', 'Ghana', 1),
-       ('Cape Coast Highway', 23, 'Cape Coast', 'Ghana', 2),
-       ('Sunyani-Accra Road', 9, 'Sunyani', 'Ghana', 3),
-       ('Mampong Road', 7, 'Kumasi', 'Ghana', 4),
-       ('Nima Street', 18, 'Accra', 'Ghana', 5),
-       ('Kumasi-Accra Road', 13, 'Techiman', 'Ghana', 6),
-       ('Wa Road', 10, 'Bolgatanga', 'Ghana', 7),
-       ('Tema-Akosombo Road', 5, 'Akosombo', 'Ghana', 8),
-       ('Tamale Road', 14, 'Navrongo', 'Ghana', 9),
-       ('Adenta Street', 21, 'Accra', 'Ghana', 10);
+INSERT INTO Sender (senderID, cust_id) VALUES
+  (1, 1),
+  (2, 2),
+  (3, 3),
+  (4, 4),
+  (5, 5),
+  (6, 6),
+  (7, 7),
+  (8, 8),
+  (9, 9),
+  (10, 10);
+  
+INSERT INTO SenderAddress (Street, houseNumber, City, senderID)
+VALUES ('Adabraka Road', 12, 'Accra', 1),
+       ('Cape Coast Highway', 23, 'Cape Coast', 2),
+       ('Sunyani-Accra Road', 9, 'Sunyani', 3),
+       ('Mampong Road', 7, 'Kumasi', 4),
+       ('Nima Street', 18, 'Accra', 5),
+       ('Kumasi-Accra Road', 13, 'Techiman', 6),
+       ('Wa Road', 10, 'Bolgatanga', 7),
+       ('Tema-Akosombo Road', 5, 'Akosombo', 8),
+       ('Tamale Road', 14, 'Navrongo', 9),
+       ('Adenta Street', 21, 'Accra', 10);
+       
+	
 
--- INSERT INTO dboSenderAddress VALUES
+-- INSERT INTO SenderAddress VALUES
    -- ( N'Outer Ring Road 1', N'OR1', N'Accra'),
     --  ( N'Ablekuma South', N'AS02', N'Tema'),
      -- ( N'1st Circular Road', N'645CR', N'Accra Central'),
@@ -232,18 +259,18 @@ VALUES ('Adabraka Road', 12, 'Accra', 'Ghana', 1),
      -- ( N'Abele Street', N'AS89', N'Community 9'),
    --   ( N'Emmause Lane', N'78EL89', N'Community 25');
    
-INSERT INTO DeliveryInformation (deliveryID, deliveryDate)
+INSERT INTO DeliveryInformation (deliveryID, deliveryDate, deliverystatus,courierId)
 VALUES
-(101, '2023-04-16'),
-(102, '2023-04-17'),
-(103, '2023-04-18'),
-(104, '2023-04-19'),
-(105, '2023-04-20'),
-(106, '2023-04-21'),
-(107, '2023-04-22'),
-(108, '2023-04-23'),
-(109, '2023-04-24'),
-(110, '2023-04-25');
+(101, '2023-04-16','in transit', 1),
+(102, '2023-04-17','at the warehouse', 2),
+(103, '2023-04-18','in transit',3),
+(104, '2023-04-19', 'delivered',4),
+(105, '2023-04-20','delivered',5),
+(106, '2023-04-21','in transit',6),
+(107, '2023-04-22','at the warehouse',7),
+(108, '2023-04-23','at the warehouse',8),
+(109, '2023-04-24','at the warehouse',9),
+(110, '2023-04-25','in transit', 10);
 
 INSERT INTO Package (packageID, deliveryID, courierId, recipientAddressID, pWeight, price)
 VALUES
@@ -259,18 +286,18 @@ VALUES
 (10, 110, 10, 310, 1.8, 30.50);
 
 
-INSERT INTO recipientAddress (recipientID, street, houseNumber, city, country)
+INSERT INTO recipientAddress (recipientID, street, houseNumber, city)
 VALUES 
-(1, 'Oxford Street', '123', 'Accra', 'Ghana'),
-(2, 'Labadi Road', '456', 'Tema', 'Ghana'),
-(3, 'Ring Road Central', '789', 'Kumasi', 'Ghana'),
-(4, 'Beach Road', '1011', 'Cape Coast', 'Ghana'),
-(5, 'North Ridge', '1213', 'Tamale', 'Ghana'),
-(6, 'Adenta-Frafraha', '1415', 'Adenta', 'Ghana'),
-(7, 'Spintex Road', '1617', 'Teshie', 'Ghana'),
-(8, 'Kwame Nkrumah Avenue', '1819', 'Sekondi-Takoradi', 'Ghana'),
-(9, 'Kofi Annan Street', '2021', 'Sunyani', 'Ghana'),
-(10, 'Asafo Roundabout', '2223', 'Koforidua', 'Ghana');
+(1, 'Oxford Street', '123', 'Accra'),
+(2, 'Labadi Road', '456', 'Tema'),
+(3, 'Ring Road Central', '789', 'Kumasi'),
+(4, 'Beach Road', '1011', 'Cape Coast'),
+(5, 'North Ridge', '1213', 'Tamale'),
+(6, 'Adenta-Frafraha', '1415', 'Adenta'),
+(7, 'Spintex Road', '1617', 'Teshie'),
+(8, 'Kwame Nkrumah Avenue', '1819', 'Sekondi-Takoradi'),
+(9, 'Kofi Annan Street', '2021', 'Sunyani'),
+(10, 'Asafo Roundabout', '2223', 'Koforidua');
 
 INSERT INTO vehicle (id, make, model, year, license_plate, type)
 VALUES
@@ -286,7 +313,7 @@ VALUES
   (10, 'Tesla', 'Model S', 2020, 'BCD890', 'Sedan');
 
 
-INSERT INTO courier (vehicleID, courierId, licenseNumber)
+INSERT INTO couriervehiclerider (vehicleID, courierId, licenseNumber)
 VALUES 
 (1, 1, 'GHA-1234567'),
 (2, 2, 'GHA-9876543'),
@@ -308,6 +335,20 @@ VALUES
   (4, 18.75, '2023-04-04', 3, 1, 4),
   (5, 10.00, '2023-04-05', 2, 5, 2);
 
+  
+  INSERT INTO courierAvailabilty (courierId, start_time,start_date,end_time,end_date) VALUES
+  (1, '09:38:00', '2021-04-23','16:24:00', '2021-04-23'),
+  (2, '08:15:00', '2021-04-25','12:40:00', '2021-04-25'),
+  (3, '09:40:00', '2021-05-13','14:10:00', '2021-05-15'),
+  (4, '09:38:00', '2021-05-14','19:02:00', '2021-05-16'),
+  (5, '10:15:00', '2023-04-01','16:50:05', '2023-04-01'),
+  (6, '11:00:00', '2022-10-10','18:38:08', '2022-10-10'),
+  (7, '08:30:00', '2021-03-12','20:00:30', '2021-03-12'),
+  (8, '09:10:00', '2022-11-23','11:13:10', '2022-11-23'),
+  (9, '10:06:00', '2021-08-20','19:30:03', '2021-08-20'),
+  (10, '09:08:00', '2021-01-30','12:40:10', '2021-01-30');
+  
+
 -- Query that keeps track of invoices and billings based on the delivery services rendered
 SELECT 
   c.cust_fname,
@@ -318,7 +359,7 @@ SELECT
   SUM(IF(p.price > 50, p.price * 0.1, 0)) as discount
 FROM 
   customer c
-  LEFT JOIN recipient r ON c.cust_id = r.customerID
+  LEFT JOIN recipient r ON c.cust_id = r.cust_id
   LEFT JOIN Package p ON r.recipientID = p.recipientAddressID
   LEFT JOIN DeliveryInformation d ON p.deliveryID = d.deliveryID
 GROUP BY 
@@ -328,14 +369,6 @@ HAVING
 ORDER BY 
   total_price DESC;
   
+  
 
--- an sql query which manages the inventory packages such as the package type, the weight and size etc
-SELECT Package.packageID, Package.pWeight, Package.price, Package.packageType, Package.packageSize
-FROM Package
-INNER JOIN Inventory ON Package.packageID = Inventory.packageID
-WHERE Package.packageSize > 10 AND Inventory.quantity > 0
-ORDER BY Package.price DESC;
-
-
-GO
-
+  
